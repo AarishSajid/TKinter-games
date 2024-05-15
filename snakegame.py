55cc5c687f71d02a62ed83c8aa4b799450086bf4
AarishSajid <161928829+AarishSajid@users.noreply.github.com>
2024-05-15 23:02:23 +0500

    Add files via upload

--- snakegame.py
+++ snakegame.py
@@ -4,8 +4,8 @@ import random
 WIDTH = 800
 HEIGHT = 600
 BG_color = "BLACK"
-FPS = 60
-SPACE_SIZE = 50
+FPS = 180
+SPACE_SIZE = 40
 S_PARTS = 4
 SNAKE_COLOR = "#FF00FF"
 FOOD_COLOR = "#00FF00"
@@ -61,10 +61,15 @@ def start_snake(snake,food):
         food = Food()
     else:
         del snake.coordinates[-1]
+        
         game_canvas.delete(snake.squares[-1])
+        
         del snake.squares[-1]
-
-    main_screen.after(FPS, start_snake ,snake , food)
+        
+    if collision(snake):
+         gameover()
+    else:
+        main_screen.after(FPS, start_snake ,snake , food)
 
 
 def changedirection(new_direction):
@@ -84,19 +89,44 @@ def changedirection(new_direction):
         direction != 'down'
         direction = new_direction
 
-def collision(snake):
+def collision(snake): 
     
-    pass
+    x , y = snake.coordinates[0]
+    
+    if  x < 0  or x >= WIDTH:
+        return True
+    elif y < 0 or y >= HEIGHT:
+        return True
 
+    for body_part in snake.coordinates[1:]:
+        if x == body_part[0] and y == body_part[1]: 
+            return True
+        
+    return False
+
+def changedirection(new_direction):
+    
+    global direction
+
+    if new_direction == 'left':
+        direction = 'left'
+    elif new_direction == 'right':
+        direction = 'right'
+    elif new_direction == 'up':
+        direction = 'up'
+    elif new_direction == 'down':
+        direction = 'down'
+    
 def gameover():
-    pass
+    game_canvas.delete(ALL)
+    game_canvas.create_text(game_canvas.winfo_width()/2, game_canvas.winfo_height()/2, font=('Poppins', 70), text="GAME OVER", fill="red", anchor=CENTER)
 
 main_screen = Tk()
 main_screen.title("Snake Game")
 main_screen.resizable(False,False)
 
 score = 0
-direction = "down"
+direction = "right"
 
 scorelabel = Label(main_screen,text=("SCORE = {}".format(score)),font=("Poppins",35))
 scorelabel.pack()
@@ -104,10 +134,22 @@ scorelabel.pack()
 game_canvas = Canvas(main_screen, height=HEIGHT , width=WIDTH ,bg=BG_color)
 game_canvas.pack()
 
-main_screen.bind('<Left>', lambda event: changedirection('left'))
-main_screen.bind('<Right>', lambda event: changedirection('right'))
-main_screen.bind('<Up>', lambda event: changedirection('up'))
-main_screen.bind('<Down>', lambda event: changedirection('down'))
+main_screen.update()
+
+window_width = main_screen.winfo_width()
+window_height = main_screen.winfo_height()
+screen_width = main_screen.winfo_screenwidth()
+screen_height = main_screen.winfo_screenheight()
+
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+
+main_screen.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+main_screen.bind('a', lambda event: changedirection('left'))
+main_screen.bind('d', lambda event: changedirection('right'))
+main_screen.bind('w', lambda event: changedirection('up'))
+main_screen.bind('s', lambda event: changedirection('down'))
 
 food = Food()
 snake = Snake()
